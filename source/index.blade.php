f1dc63adb3f9e2eba1d1aba075c23f94c8697215
aaavwclub <51654108+aaavwclub@users.noreply.github.com>
2021-08-10 15:10:54 -0600

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,7 +4,7 @@
     <h1>Logos for AAAVW/RT66 Air-Cooled VW Club</h1>
 
     <h2> 20th Anniversary Logo</h2>
-    <img src="../images/AAAVW_RT66_Logo_3750x3750px.png"/>
+    <img src="https://res.cloudinary.com/dn9akjdfi/image/upload/v1628629831/AAAVW_RT66_Logo_3750x3750px_vutcli.png"/>
 
 
 @endsection
